2b8009e0f1a19eb99f65fac19390b064b6ca62ef
Alisa Ryan Herr <aherr@email.unc.edu>
2017-10-10 13:02:50 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,27 @@
 
 // ** MySQL settings ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+define( 'DB_NAME', getenv('DATABASE_NAME') );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', getenv('DATABASE_USER') );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', getenv('DATABASE_PASSWORD') );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST',  getenv(strtoupper(str_replace('-', '_', getenv('DATABASE_SERVICE_NAME'))).'_SERVICE_HOST') );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+
+if($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'){
+    $_SERVER['HTTPS'] = 'on';
+    $_SERVER['SERVER_PORT'] = 443;
+}
 
 /**
  * Authentication Unique Keys and Salts.
@@ -46,14 +51,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'R0tNbp6GAXfqpQ512ssWpUPoQDysLlM53RtRQNA1RCKLkg3qf0Vxtj36FrS7skY3QM5VXNsA7l/D5bfzld5Gfw==');
-define('SECURE_AUTH_KEY',  'Cx61YoOdLY83lgjdIK+EGTS3bA6Vm4Z/Rv2RHYRSDHLn5GJrURkkBfwEbF08KJQMYhzm5ARrbc9/cUy4Zvt1Cg==');
-define('LOGGED_IN_KEY',    'a3R/P8fbk0F93VoGgmDGX5g2QDt5ONYS0nvPfokFS31ZcO/HrzCLp2CkSlhc9SqVi13dbFeid/8kBiCFb1voZQ==');
-define('NONCE_KEY',        'Dmeom6pzuaBzRVbx/HF4qHo7OptlsB/9zqB/0PvfmRsmbS5xUlHyRsmL+UQL4uqhm3pbCcVmyYa8yCITI4HI4g==');
-define('AUTH_SALT',        'RbeDNcZ7Oc1qPHfmLdhqWfNVEXqEO8zlTP7V1DSBQlDVx5n9A+iHEewifKwqTg9GPcyvZUJoKHUj/GuGoJ4V9Q==');
-define('SECURE_AUTH_SALT', 'hlqMuow+PFWWvRq6gs5/4XNOFHFGlI3IM1HCy0beQ8rkq2gJd+efMjO+OsgfkPxIuFxv5Yaexcq36vTEf2VALA==');
-define('LOGGED_IN_SALT',   'G5MVMoYP1AoToPH5ctuumXJEtRu2GkaICc5WvFEMn1ewYUXj7Wb1eLEKjWxn01VBYx17diP8o3t4GJGc8tf2LA==');
-define('NONCE_SALT',       'wbc+u9Hid5UAPHCinc7uCIX4VL10ofxd37oA1G3/xMbA8lzOqlGq3CtnSfmg39kSjRQJyzjodYsA4+PuPH/yJQ==');
+define('AUTH_KEY',         ' 5 ri.u:kNG~0T)nSHL)IjegkOI!i;puL-94-j:g`G1FyFOk2 ^qy[?md{3}$V(>');
+define('SECURE_AUTH_KEY',  '$mSDhQ%v;c7wZ+L7|Yu:t&YNvS>^4@wT,<{^5^@QFk4kk6SW~ yks+w(!=4[]cK4');
+define('LOGGED_IN_KEY',    'WX9F<Q&4+k;;O=k`myNDvuOg_PJQ}|Hh!1B|;2F?6MbzTN:MDwjg!tb)lz>v`-oN');
+define('NONCE_KEY',        'G3e?_j|Z WU%Mv![55R$Lo7a-5lGZBBdi=RuWV;Ks-U94e.EI5!*m%r< yDR.-dR');
+define('AUTH_SALT',        '^&k0aj5B|JG57rKv<v)~O)|U / bSTE29|X9^IAxjcIKn|v2? 5^wZE|+uL=E7H7');
+define('SECURE_AUTH_SALT', '/|)9o;w7gP:m;KA!n.b%ogEs=YTHc`dbMx<@:m<3Izs8YaM3-r.}RNt(QUsfphz)');
+define('LOGGED_IN_SALT',   ';-lK&,[@(^c>pQtM.lmAC+3]Q>`%*| Ih@$ik(|=Ach#1NhO9=+hXC#.|G-S)Kd;');
+define('NONCE_SALT',       'CD9p:X!e.+c&AI5VqUzh;AcrTHw4%B+{h@!t.$0uDz;A0W|o*zasdixT;&4y?0eG');
 
 /**
  * WordPress Database Table prefix.
@@ -64,13 +69,6 @@ define('NONCE_SALT',       'wbc+u9Hid5UAPHCinc7uCIX4VL10ofxd37oA1G3/xMbA8lzOqlGq
 $table_prefix = 'wp_';
 
 
-
-
-
-/* Inserted by Local by Flywheel. See: http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy */
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
-}
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
