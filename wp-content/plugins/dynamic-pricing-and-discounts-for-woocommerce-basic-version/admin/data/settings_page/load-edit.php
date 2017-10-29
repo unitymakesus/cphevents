3d3149e1c60422f9a6c63ecc27ef24d900451659
Alisa Herr <alisa@unitymakes.us>
2017-10-29 10:27:27 -0400

    Remove unused plugins

--- wp-content/plugins/dynamic-pricing-and-discounts-for-woocommerce-basic-version/admin/data/settings_page/load-edit.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
-}
-//$_REQUEST=get_option('xa_dynamic_pricing_setting');
--- wp-content/plugins/dynamic-pricing-and-discounts-for-woocommerce-basic-version/admin/js/xa-dynamic-pricing-plugin-admin.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function ($) {
-    //jQuery('#product_id').trigger( 'wc-enhanced-select-init' );
-    //jQuery('#product_id').select2();
-}); 
