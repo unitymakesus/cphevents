0b0cbffbdcbb23298bba592ca0653fd7777214d9
Alisa Herr <alisa@unitymakes.us>
2017-11-30 21:24:31 -0500

    Attempt to handle posting URL response data from TouchNet

--- wp-content/themes/storefront-cph/page-response.php
+++ wp-content/themes/storefront-cph/page-response.php
@@ -5,3 +5,31 @@
  * It collects the parameters describing the transaction from uPay and updates
  * the corresponding order accordingly.
  */
+
+$pmt_status = $_POST['pmt_status'];
+$EXT_TRANS_ID = $_POST['EXT_TRANS_ID'];
+
+// Set up order object
+$order = new WC_Order($_POST['EXT_TRANS_ID']);
+
+// Handle order status
+if ($_POST['pmt_status'] == 'success') {
+	$order->add_order_note('TouchNet payment completed');
+	$order->payment_complete();
+} elseif ($_POST['pmt_status'] == 'cancelled') {
+	$order->add_order_note('TouchNet payment cancelled');
+}
+
+// Save post stuff as order meta
+if ( ! empty( $_POST['pmt_status'] ) )
+	update_post_meta( $order_id, 'pmt_status', $_POST['pmt_status'] );
+if ( ! empty( $_POST['tpg_trans_id'] ) )
+	update_post_meta( $order_id, 'tpg_trans_id', $_POST['tpg_trans_id'] );
+if ( ! empty( $_POST['pmt_amt'] ) )
+	update_post_meta( $order_id, 'pmt_amt', $_POST['pmt_amt'] );
+if ( ! empty( $_POST['pmt_status'] ) )
+	update_post_meta( $order_id, 'pmt_status', $_POST['pmt_status'] );
+if ( ! empty( $_POST['pmt_date'] ) )
+	update_post_meta( $order_id, 'pmt_date', $_POST['pmt_date'] );
+if ( ! empty( $_POST['sys_tracking_id'] ) )
+	update_post_meta( $order_id, 'sys_tracking_id', $_POST['sys_tracking_id'] );
